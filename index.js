ce296ef06502fbcf4018a8f6b88451788e3b7133
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-12-12 01:40:44 +0530

    updated required tasks

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get("/", (_, res) => {
 
 const messages = [];
 
+app.get("/findMessages", (req, res) => {
+  try {
+    return res.status(200).json(messages);
+  } catch (err) {
+    console.error(`Error occurred in file: index, function: get -`, err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.post("/message", (req, res) => {
+  try {
+    const { user, text } = req.body;
+    if (!user || !text)
+      return res.status(400).json({ error: "Please provide a valid input" });
+    const message = { user, text, timestamp: new Date().toISOString() };
+    messages.push(message);
+    return res.status(200).json({ message });
+  } catch (err) {
+    console.error(`Error occurred in file: index, function: post -`, err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
